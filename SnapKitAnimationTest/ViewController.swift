13a1888018240754b7e758c9a04b77750cc69651
eniPay Developers <developers@enipay.com>
2015-10-20 23:25:56 +0100

    initial commit

--- SnapKitAnimationTest/ViewController.swift
+++ SnapKitAnimationTest/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    var animatingView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setupViews()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        self.layoutForSecondPosition()
+        
+        UIView.animateWithDuration(1) { () -> Void in
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func setupViews() {
+        self.view.addSubview(self.animatingView)
+        
+        self.customsie()
+        self.layoutForFirstPosition()
     }
 
-
+    func customsie() {
+        self.animatingView.backgroundColor = .blackColor()
+    }
+    
+    func layoutForFirstPosition() {
+        self.animatingView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.view.snp_top).offset(50)
+            make.leading.equalTo(self.view.snp_leading).offset(50)
+            make.height.equalTo(50)
+            make.width.equalTo(50)
+        }
+    }
+    
+    func layoutForSecondPosition() {
+        self.animatingView.snp_remakeConstraints { (make) -> Void in
+            make.centerX.equalTo(self.view.snp_centerX)
+            make.centerY.equalTo(self.view.snp_centerY)
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+        }
+    }
 }
 
